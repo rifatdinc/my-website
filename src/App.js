54bcb8acca8ce566e25ebea0c91e3a3fc21ddb7c
Rifat Dinc <rafidinc41@gmail.com>
2022-10-28 09:19:12 +0300

    001 First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul className="navbarParent">
+          <li> <a href=""> Ana Sayfa  </a> </li>
+          <li> <a href=""> Hakkımda   </a> </li>
+          <li> <a href=""> Blog       </a> </li>
+          <li> <a href=""> Seyehatlerim </a> </li>
+          <li> <a href=""> Rifa tDinc</a> </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
